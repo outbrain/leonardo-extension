5ae7fb359df84419ab388d90c904ca4bcdeedf76
Ron Gross <chiefninjaofficer@gmail.com>
2016-12-07 12:15:17 +0200

    merge

--- src/background.js
+++ src/background.js
@@ -1,5 +1,3 @@
-
-<<<<<<< HEAD
 function invokeToggle() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {func: "toggleEnabled"}, function(response) {
@@ -23,20 +21,14 @@ function invokeToggle() {
             chrome.browserAction.setIcon({path: newIcon});
         });
     });
-=======
+}
+
 function showLeonardo(status) {
   if (status) {
     chrome.tabs.executeScript(null, {'file': 'src/loader.js'});
   } else {
     chrome.tabs.executeScript(null, {'file': 'src/unloader.js'});
   }
-}
-
-function toggleEnabled() {
-  enabled = !enabled;
-  showLeonardo(enabled);
-  chrome.browserAction.setIcon({path: enabled ? 'icon.png' : 'icon-off.png'});
->>>>>>> 7e4f301044ff5ea8afbf24f274bc362a30406206
 }
 
 chrome.browserAction.onClicked.addListener(invokeToggle);
