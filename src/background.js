c0d76e95bab7ea9ff7410d419a28916195be12dd
Ron Gross <chiefninjaofficer@gmail.com>
2016-12-07 11:17:41 +0200

    Click disable/enbale leo

--- src/background.js
+++ src/background.js
@@ -7,4 +7,26 @@ function inject() {
   document.head.appendChild(script);
 }
 
+var enabled;
+function toggleEnabled() {
+    var newIcon;
+    if (enabled) {
+        console.log("Disabling Leo");
+        enabled = false;
+        newIcon = 'icon-off.png';
+    } else {
+        console.log("Enabling Leo");
+        enabled = true;
+        newIcon = 'icon.png';
+    }
+    
+    chrome.browserAction.setIcon({
+            path: newIcon
+//        ,
+//            tabId: sender.tab.id
+        });
+}
+
+chrome.browserAction.onClicked.addListener(toggleEnabled);
+
 // inject();
