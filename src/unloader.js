1ce540ec218c17715a55290b00e2e622d695d0f8
Dan Revah <drevah@outbrain.com>
2016-12-07 12:55:39 +0200

    fix

--- src/unloader.js
+++ src/unloader.js
@@ -1,8 +1,9 @@
 
 var leonardoElm = document.querySelector('.leonardo-launcher');
 
+window.postMessage('leonardo:toggle:states', '*');
+
 if (leonardoElm && leonardoElm.parentNode && leonardoElm.parentNode.style) {
   leonardoElm.parentNode.style.display = 'none';
 }
 
-window.postMessage('leonardo:toggle:states', '*');
